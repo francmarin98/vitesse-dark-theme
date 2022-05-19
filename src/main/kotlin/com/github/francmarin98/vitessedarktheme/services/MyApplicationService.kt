cd1c3565b8380df0d9e2e632e4ef7f0a6cbfa11a
GitHub Action <action@github.com>
2022-05-19 14:32:39 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/francmarin98/vitessedarktheme/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.francmarin98.vitessedarktheme.services
+
+import com.github.francmarin98.vitessedarktheme.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
